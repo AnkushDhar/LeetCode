97328afdfe5577f87c12c336976bec50542eceeb
Ankush Dhar <124507653+AnkushDhar@users.noreply.github.com>
2023-08-05 23:18:52 +0530

    Time: 1 ms (99.88%), Space: 42.9 MB (99.88%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+        if (start > end) {
+            result.add(null);
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = generateTrees(start, i - 1);
+            List<TreeNode> rightSubtrees = generateTrees(i + 1, end);
+
+            for (TreeNode leftSubtree : leftSubtrees) {
+                for (TreeNode rightSubtree : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftSubtree;
+                    root.right = rightSubtree;
+                    result.add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
