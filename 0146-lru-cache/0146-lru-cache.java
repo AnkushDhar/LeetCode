6c42bb5a7cf97e3aab4f16853786ebcade6f5a6b
Ankush Dhar <124507653+AnkushDhar@users.noreply.github.com>
2023-08-30 12:39:57 +0530

    Time: 37 ms (98.80%), Space: 120.2 MB (59.50%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,70 @@
+class LRUCache {
+    private final int capacity;
+    private final Map<Integer, Node> cache;
+    private final Node head;
+    private final Node tail;
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            moveToHead(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
+            moveToHead(node);
+        } else {
+            if (cache.size() >= capacity) {
+                Node tailPrev = tail.prev;
+                removeNode(tailPrev);
+                cache.remove(tailPrev.key);
+            }
+            Node newNode = new Node(key, value);
+            addToHead(newNode);
+            cache.put(key, newNode);
+        }
+    }
+}
